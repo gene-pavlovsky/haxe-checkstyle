e4f2d1c953e2d89e72dd25847145ddde2e1c9228
Christoph Otter <christophotter@gmail.com>
2018-03-21 14:47:28 +0100

    Add TypeHeaderWrapCheck with optional indentation checking

---
+++ src/checkstyle/checks/whitespace/TypeHeaderWrapCheck.hx
@@ -0,0 +1,64 @@
+package checkstyle.checks.whitespace;
+
+import checkstyle.utils.TokenTreeCheckUtils;
+
+@name("TypeHeaderWrap")
+@desc("Checks line wrapping of extends and implements keywords.")
+class TypeHeaderWrapCheck extends WrapCheckBase {
+
+	public var forceIndent:Bool;
+	
+	public function new() {
+		super();
+		option = NL;
+		forceIndent = false;
+		tokens = [
+			"extends",
+			"implements"
+			//That's it for now
+		];
+	}
+
+	override function actualRun() {
+		var tokenList:Array<TokenDef> = [];
+
+		if (hasToken("extends")) tokenList.push(Kwd(KwdExtends));
+		if (hasToken("implements")) tokenList.push(Kwd(KwdImplements));
+
+		if (tokenList.length <= 0) return;
+		checkTokens(tokenList);
+	}
+	
+	override function checkTokens(tokenList:Array<TokenDef>) 
+	{
+		super.checkTokens(tokenList);
+		
+		var root:TokenTree = checker.getTokenTree();
+		var allTokens:Array<TokenTree> = root.filter(tokenList, ALL);
+
+		for (tok in allTokens) {
+			if (isPosSuppressed(tok.pos)) continue;
+			if (TokenTreeCheckUtils.isTypeParameter(tok)) continue;
+			if (TokenTreeCheckUtils.isImportMult(tok)) continue;
+			if (TokenTreeCheckUtils.filterOpSub(tok)) continue;
+
+			var linePos:LinePos = checker.getLinePos(tok.pos.min);
+			var parentLine:String = tok.parent.tok != null ? checker.lines[checker.getLinePos(tok.parent.pos.max).line] : "";
+			var nextLine:String = linePos.line + 1 < checker.lines.length ? checker.lines[linePos.line + 1] : "";
+			var whitespaceRE:EReg = ~/(^\s*)/;
+			whitespaceRE.match(parentLine);
+			var parentIndention = whitespaceRE.matched(1).length;
+			whitespaceRE.match(nextLine);
+			var nextLineIndention = whitespaceRE.matched(1).length;
+			
+			if (forceIndent && option == NL && linePos.ofs <= parentIndention) {
+				logPos('Token "$tok" must be indented more', tok.pos);
+				continue;
+			}
+			if (forceIndent && option == EOL && nextLineIndention <= parentIndention ) {
+				logPos('Next line must be indented more', tok.pos);
+				continue;
+			}
+		}
+	}
+}
---
+++ test/checks/whitespace/TypeHeaderWrapCheckTest.hx
@@ -0,0 +1,76 @@
+package checks.whitespace;
+
+import checkstyle.checks.whitespace.TypeHeaderWrapCheck;
+
+class TypeHeaderWrapCheckTest extends CheckTestCase<TypeHeaderWrapCheckTests> {
+
+	static inline var MSG_EXTENDS_EOL:String = 'Token "extends" must be at the end of the line';
+	static inline var MSG_IMPLEMENTS_EOL:String = 'Token "implements" must be at the end of the line';
+	static inline var MSG_EXTENDS_NL:String = 'Token "extends" must be on a new line';
+	static inline var MSG_IMPLEMENTS_NL:String = 'Token "implements" must be on a new line';
+	static inline var MSG_INDENTATION_NL_IMPLEMENTS:String = 'Token "implements" must be indented more';
+	
+	
+	public function testCorrectWrap() {
+		var check = new TypeHeaderWrapCheck();
+		assertNoMsg(check, CORRECT_NL_WRAP);
+		assertNoMsg(check, MISSING_INDENTATION_NL_WRAP_IMPLEMENTS);
+	}
+
+	public function testIncorrectWrap() {
+		var check = new TypeHeaderWrapCheck();
+		assertMsg(check, CORRECT_EOL_WRAP_EXTENDS, MSG_EXTENDS_NL);
+		assertMsg(check, INCORRECT_NL_WRAP_IMPLEMENTS, MSG_IMPLEMENTS_NL);
+	}
+
+	public function testOptionEOL() {
+		var check = new TypeHeaderWrapCheck();
+		check.option = EOL;
+		assertNoMsg(check, CORRECT_EOL_WRAP);
+		assertMsg(check, CORRECT_EOL_WRAP_EXTENDS, MSG_IMPLEMENTS_EOL);
+	}
+	
+	public function testForceIndent() {
+		var check = new TypeHeaderWrapCheck();
+		check.forceIndent = true;
+		assertNoMsg(check, CORRECT_NL_WRAP);
+		assertMsg(check, CORRECT_EOL_WRAP_EXTENDS, MSG_EXTENDS_NL);
+		assertMsg(check, MISSING_INDENTATION_NL_WRAP_IMPLEMENTS, MSG_INDENTATION_NL_IMPLEMENTS);
+	}
+}
+
+@:enum
+abstract TypeHeaderWrapCheckTests(String) to String {
+	var CORRECT_NL_WRAP = "
+	class Test
+		extends A
+		implements B
+	{
+	}";
+	
+	var CORRECT_EOL_WRAP = "
+	class Test extends
+		A implements
+		B
+	{
+	}";
+
+	var CORRECT_EOL_WRAP_EXTENDS = "
+	class Test extends
+		A 
+		implements B
+	{
+	}";
+	
+	var INCORRECT_NL_WRAP_IMPLEMENTS = "
+	class Test extends A implements
+		B
+	{
+	}";
+
+	var MISSING_INDENTATION_NL_WRAP_IMPLEMENTS = "
+	class Test
+	implements Test2 {
+		
+	}";
+}
